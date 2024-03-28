eeb9d99671cbca71c275f9f5b5e0112f276122e1
Michael Brandmeyer <39311170+bmichael99@users.noreply.github.com>
2024-03-28 10:58:58 -0700

    Add files via upload
    Fixed an issue. Now fully functional.

--- RatemyProfessorAPI/rateMyProfessorAPI.py
+++ RatemyProfessorAPI/rateMyProfessorAPI.py
@@ -1,18 +1,14 @@
 import ratemyprofessor
 from flask import Flask, request, jsonify
 from urllib.parse import unquote
+import time
 
 def ratemyProfessorAPI(school_name,prof_name):
-    #school_name = unquote(school_name)
-    #prof_name = unquote(prof_name)
-    print(str(school_name))
-    print(str(prof_name))
-    getProfessor = ratemyprofessor.get_professor_by_school_and_name(
-        ratemyprofessor.get_school_by_name("California State University San Marcos"), "Lanier Castillo")
+    getProfessor = ratemyprofessor.get_professor_by_school_and_name(ratemyprofessor.get_school_by_name(str(school_name)), str(prof_name))
     if getProfessor is not None:
         return(getProfessor)
 
-def testAPI():
+def testAPI(): #Not used, call if needed
     getProfessor = ratemyprofessor.get_professor_by_school_and_name(
         ratemyprofessor.get_school_by_name("California State University San Marcos"), "Lanier Castillo")
     if getProfessor is not None:
@@ -29,18 +25,17 @@ def testAPI():
 
 
 
-app = Flask(__name__)
+app = Flask(__name__.split('.')[0])
 
 
 
 @app.route('/get_Professor', methods=['GET'])
 def get_Professor():
-    professor_name = request.args.get('professor')
-    school_name = request.args.get('school')
-    getProfessor = ratemyProfessorAPI(professor_name,school_name)
-    print("test" + getProfessor.name)
-    professor = [{'name': getProfessor.name, 'department': getProfessor.department, 'school': getProfessor.school.name, 
-              'rating' : getProfessor.rating, 'difficulty' : getProfessor.difficulty, 'totalratings' : getProfessor.num_ratings, 'would_take_again' : round(getProfessor.would_take_again, 1)}]
+    professor_name = str(request.args.get('professor'))
+    school_name = str(request.args.get('school'))
+    getProfessor = ratemyProfessorAPI(school_name,professor_name)
+    professor = {'name': getProfessor.name, 'department': getProfessor.department, 'school': getProfessor.school.name, 
+              'rating' : getProfessor.rating, 'difficulty' : getProfessor.difficulty, 'totalratings' : getProfessor.num_ratings, 'would_take_again' : round(getProfessor.would_take_again, 1)}
     return jsonify({'professor': professor})
 
 if __name__ == '__main__':
